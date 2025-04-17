fc88034b725adaa080e68e7f015667ba48e2a7b1
qwdkjj <baaarhom4@gmail.com>
2025-04-17 15:12:50 +0300

    alot changes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'expires_in' => config('jwt.ttl') * 60
+        ]);
+    }
+
+
+    public function check()
+    {
+        dd("success");
+    }
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+
 abstract class Controller
 {
 
 
 
-
-
-    //
-    //
-    //
 }
